7a11467ef0a03423621f0eff25767e8da4199fc0
fahimulkabir <hamimkabir14@gmail.com>
2026-06-20 19:14:44 -0500

    path modification and github auth config

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,28 @@ import News from "./pages/News";
 // import Projects from "./pages/Projects";
 // import Contact from "./pages/Contact";
 
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         {/* Layout Route */}
+//         <Route element={<MainLayout />}>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/publications" element={<Publications />} />
+//           <Route path="/research" element={<Research />} />
+//           <Route path="/members" element={<Members />} />
+//           <Route path="/news" element={<News />} />
+//           {/* <Route path="/contact" element={<Contact />} /> */}
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
 export default function App() {
+  // We are passing the Vite base URL to the router
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         {/* Layout Route */}
         <Route element={<MainLayout />}>
